df44bd2d3672d1a088681e5acdc1c5a5ec8a46a2
ta-ku-ya <108854454+ta-ku-ya@users.noreply.github.com>
2022-08-22 13:41:10 +0000

    [Add]アソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :installs
+  root to: 'homes#top'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'homes/about' => 'homes#about', as: :about
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
 end
