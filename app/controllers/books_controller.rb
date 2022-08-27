347147fc5b8c98bec137b85dbba77f822ffaf1af
ta-ku-ya <108854454+ta-ku-ya@users.noreply.github.com>
2022-08-27 13:26:16 +0000

    [Add]indexページ右側作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,36 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+
   end
-  
+
+  def new
+    @book = Book.new
+  end
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
+
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Bool.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'homes/about' => 'homes#about', as: :about
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :edit, :update,:index, :show, :destroy]
   resources :users, only: [:show, :edit]
 end
